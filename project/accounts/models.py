cfc578d866a937859047a85652773e5914ad2474
Mohammadbaraftab <136730537+Mohammadbaraftab@users.noreply.github.com>
2025-10-01 09:10:56 +0330

    add and modified some files

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from.managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.CharField(verbose_name="email address", max_length=200, unique=True)
+    phone_number = models.CharField(verbose_name= "phone numbers", max_length=11, unique=True)
+    full_name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email", "full_name"]
+
+    def __str__(self):
+        return self.email
+    
+    def has_prem(delf, perm, object = None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
